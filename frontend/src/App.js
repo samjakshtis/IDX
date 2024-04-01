bd2f5e6ebbfed7929010bfc156e477d5a47f80db
samueljakshtis <sam.jakshtis@gmail.com>
2024-03-31 20:11:45 -0400

    I did a lot of changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import WorkingPapers from './components/WorkingPapers/WorkingPapers.js';
 import Header from './components/Header/Header.js';
@@ -6,9 +6,95 @@ import Home from './components/Home/Home.js';
 import Movies from './components/Movies/Movies.js';
 import Podcasts from './components/Podcasts/Podcasts.js';
 import './App.css'
-import MouseSand from './components/MouseTrail/mousetrail.js';
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
 
 function App() {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  const options = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: "#E9F2F7",
+        },
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#0610CC",
+        },
+        links: {
+          color: "#000000",
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        move: {
+          direction: "none",
+          enable: true,
+          outModes: {
+            default: "bounce",
+          },
+          random: false,
+          speed: 6,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 80,
+        },
+        opacity: {
+          value: 0.5,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 5 },
+        },
+      },
+      detectRetina: true,
+    }),
+    [],
+  );
+
   return (
     <BrowserRouter>
       <div className="App">
@@ -20,7 +106,13 @@ function App() {
           <Route path="/podcasts" element={<Podcasts />} />
           {/* <Route path="/contact" element={<Contact />} /> */}
         </Routes>
-        <MouseSand />
+        <div className='particles'>
+          <Particles
+            id="tsparticles"
+            particlesLoaded={particlesLoaded}
+            options={options}
+          />
+        </div>
       </div>
     </BrowserRouter>
   );
